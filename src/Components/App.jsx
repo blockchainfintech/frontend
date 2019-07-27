6ac4decddade91bfd431729b3d3403dda5285b15
Eddie Qi <eddieqi128@gmail.com>
2019-07-28 01:41:20 +1000

    refactor into separate files

---
+++ src/Components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Heatmap from './Heatmap'
+import '../Style/App.css';
+
+class App extends React.Component {
+	render() {
+		return (
+			<Heatmap />
+		);
+	}
+
+}
+
+export default App;
